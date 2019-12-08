9ccc1305a8817f7bf8c4fa2949ad13bc27bf2fa2
Half's Inner <kajbrz@hotmail.com>
2019-12-08 17:52:21 +0100

    5th. I

---
+++ scripts/05_I.py
@@ -0,0 +1,192 @@
+'''
+--- Day 5: Sunny with a Chance of Asteroids ---
+You're starting to sweat as the ship makes its way toward Mercury. The Elves suggest that you get the air conditioner working by upgrading your ship computer to support the Thermal Environment Supervision Terminal.
+
+The Thermal Environment Supervision Terminal (TEST) starts by running a diagnostic program (your puzzle input). The TEST diagnostic program will run on your existing Intcode computer after a few modifications:
+
+First, you'll need to add two new instructions:
+
+Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
+Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+Programs that use these instructions will come with documentation that explains what should be connected to the input and output. The program 3,0,4,0,99 outputs whatever it gets as input, then halts.
+
+Second, you'll need to add support for parameter modes:
+
+Each parameter of an instruction is handled based on its parameter mode. Right now, your ship computer already understands parameter mode 0, position mode, which causes the parameter to be interpreted as a position - if the parameter is 50, its value is the value stored at address 50 in memory. Until now, all parameters have been in position mode.
+
+Now, your ship computer will also need to handle parameters in mode 1, immediate mode. In immediate mode, a parameter is interpreted as a value - if the parameter is 50, its value is simply 50.
+
+Parameter modes are stored in the same value as the instruction's opcode. The opcode is a two-digit number based only on the ones and tens digit of the value, that is, the opcode is the rightmost two digits of the first value in an instruction. Parameter modes are single digits, one per parameter, read right-to-left from the opcode: the first parameter's mode is in the hundreds digit, the second parameter's mode is in the thousands digit, the third parameter's mode is in the ten-thousands digit, and so on. Any missing modes are 0.
+
+For example, consider the program 1002,4,3,4,33.
+
+The first instruction, 1002,4,3,4, is a multiply instruction - the rightmost two digits of the first value, 02, indicate opcode 2, multiplication. Then, going right to left, the parameter modes are 0 (hundreds digit), 1 (thousands digit), and 0 (ten-thousands digit, not present and therefore zero):
+
+ABCDE
+ 1002
+
+DE - two-digit opcode,      02 == opcode 2
+ C - mode of 1st parameter,  0 == position mode
+ B - mode of 2nd parameter,  1 == immediate mode
+ A - mode of 3rd parameter,  0 == position mode,
+                                  omitted due to being a leading zero
+This instruction multiplies its first two parameters. The first parameter, 4 in position mode, works like it did before - its value is the value stored at address 4 (33). The second parameter, 3 in immediate mode, simply has value 3. The result of this operation, 33 * 3 = 99, is written according to the third parameter, 4 in position mode, which also works like it did before - 99 is written to address 4.
+
+Parameters that an instruction writes to will never be in immediate mode.
+
+Finally, some notes:
+
+It is important to remember that the instruction pointer should increase by the number of values in the instruction after the instruction finishes. Because of the new instructions, this amount is no longer always 4.
+Integers can be negative: 1101,100,-1,4,0 is a valid program (find 100 + -1, store the result in position 4).
+The TEST diagnostic program will start by requesting from the user the ID of the system to test by running an input instruction - provide it 1, the ID for the ship's air conditioner unit.
+
+It will then perform a series of diagnostic tests confirming that various parts of the Intcode computer, like parameter modes, function correctly. For each test, it will run an output instruction indicating how far the result of the test was from the expected value, where 0 means the test was successful. Non-zero outputs mean that a function is not working correctly; check the instructions that were run before the output instruction to see which one failed.
+
+Finally, the program will output a diagnostic code and immediately halt. This final output isn't an error; an output followed immediately by a halt means the program finished. If all outputs were zero except the diagnostic code, the diagnostic program ran successfully.
+
+After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program produce?
+'''
+
+import sys
+from collections import deque
+
+class Virtual_Machine:
+    
+    def __init__(self, int_code, program_alarm=False, noun=12, verb=2, debug=False):
+        self.__debug_mode = debug
+        if self.__debug_mode:
+            print('Debug Mode...')
+        
+        self.__int_code = int_code
+        self.__is_running = True
+        self.__pc = 0
+        self.__last_pc = self.__pc
+        self.__step_counter = 0
+        self.__arg_mode_stack = deque()
+        if program_alarm:
+            self.__int_code[1] = noun
+            self.__int_code[2] = verb
+        
+    def is_running(self):
+        return self.__is_running
+        
+    def step(self):
+        self.__last_pc = self.__pc
+        operate_length = self.__operate()
+        self.__pc += operate_length
+        self.__step_counter += 1
+        
+    def first_position(self):
+        first_pos = 0
+        return self.__int_code[first_pos]
+        
+    def print_debug_info(self):
+        self.__debug('')
+        self.__debug(' IntCode\n {}'.format(self.__int_code))
+        self.__debug('Is Running {}'.format(self.__is_running))
+        self.__debug('        PC {}'.format(self.__pc))
+        self.__debug('   LAST PC {}'.format(self.__last_pc))
+        self.__debug('     Steps {}'.format(self.__step_counter))
+        self.__debug(' arg modes {}'.format(self.__arg_mode_stack))
+        
+    def __operate(self):
+        opcode = self.__int_code[self.__pc] % 100
+        self.__arg_mode_stack.clear()
+        modes = self.__int_code[self.__pc] // 100
+        for arg in range(3):
+            arg_mode = modes % 10
+            modes //= 10
+            self.__arg_mode_stack.appendleft(bool(arg_mode))   
+        
+        self.__debug('O({})'.format(opcode))
+        return \
+        {  
+            1  : self.__add,
+            2  : self.__multiple,
+            3  : self.__input,
+            4  : self.__print,
+            99 : self.__exit 
+        }[opcode]()
+    
+    def __add(self):            
+        arg1 = self.__read_arg()
+        arg2 = self.__read_arg()
+        self.__write_arg(arg1 + arg2)
+        return 1
+    
+    def __multiple(self):   
+        arg1 = self.__read_arg()
+        arg2 = self.__read_arg()        
+        self.__write_arg(arg1 * arg2)
+        return 1
+
+    def __input(self): 
+        val = 0
+        try:
+            val = int(input())
+        except:
+            pass
+        if not (0 <= val <= 99):
+            raise Exception('Passed value \'{}\' is not in range [{}, {}]', val, 0, 99)
+            
+        self.__write_arg(val)
+        return 1
+
+    def __print(self):
+        arg1 = self.__read_arg()
+        print(arg1, end='')
+        return 1
+    
+    def __exit(self):
+        self.__is_running = False
+        return 1
+        
+    # Helpers
+    def __read_arg(self):
+        self.__pc += 1        
+        if self.__pc >= len(self.__int_code):
+            raise Exception('Segmentation fault')
+        
+        ret = 0
+        if self.__arg_mode_stack.pop():
+            ret = self.__int_code[self.__pc]
+        else:
+            ret = self.__int_code[self.__int_code[self.__pc]]
+            
+        return ret    
+
+    def __write_arg(self, val):
+        self.__pc += 1
+        if self.__pc >= len(self.__int_code):
+            raise Exception('Segmentation fault')
+        
+        if self.__arg_mode_stack.pop():
+            self.__int_code[self.__pc] = val
+        else:
+            self.__int_code[self.__int_code[self.__pc]] = val
+            
+    def __debug(self, str):
+        if self.__debug_mode:
+            print('D:{}'.format(str))
+
+def parse_file(file_path : str):
+    int_code = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            int_code.extend(map(int, line.replace('\n', '').split(',')))
+    
+    return int_code
+
+def main(argv):
+    vm = Virtual_Machine(parse_file(argv[1]), debug=True)
+    while (vm.is_running()):
+        try:
+            vm.step()
+        except:
+            break
+    vm.print_debug_info()
+    print('First Position Value = {}'.format(vm.first_position()))
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+    
